2e7b377f211680064927677212db856e47817b23
dgupta777 <darshitgupta.cse20@jecrc.ac.in>
2018-09-08 19:38:04 +0530

    added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 (function() {
 
-                var welcomeSection = $('.welcome-section'),
-                    enterButton = welcomeSection.find('.enter-button');
+  setTimeout(function() {
+    $('.fly-in-text1').removeClass('hidden');
+  }, 1500);
 
-                setTimeout(function() {
-                    welcomeSection.removeClass('content-hidden');
-                }, 500);
-                setTimeout(function() {
-                    here.removeClass('content-hidden');
-                }, 500);
+  setTimeout(function() {
+    $('.fly-in-text2').removeClass('hidden');
+  }, 4000);
 
-                enterButton.on('click', function(e) {
-                    e.preventDefault();
-                    welcomeSection.addClass('content-hidden').fadeOut();
-                });
+  setTimeout(function() {
+    $('.here').removeClass('hidden');
+  }, 6000);
 
-                setTimeout(function() {
-                    $('.fly-in-text2').removeClass('hidden');
-                }, 1500);
+  setTimeout(function() {
+    $('.about').removeClass('hidden');
+  }, 7000);
 
-                setTimeout(function() {
-                    $('.here').removeClass('hidden');
-                }, 3000);
+  setTimeout(function() {
+    $('.enter-button').removeClass('hidden');
+  }, 8000);
 
-                setTimeout(function() {
-                    $('.about').removeClass('hidden');
-                }, 4000);
-
-
-            })();
+})();
